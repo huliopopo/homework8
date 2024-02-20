602166ab4cc24012fda3725d23f8ba7af7e7e8a8
huliopopo <yashin.hlpp@gmail.com>
2024-02-20 19:27:15 +0300

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,58 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //task1
-        int[] arr1 = new int[]{1, 2, 3, 55, 100, 101};
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
         double[] arr2 = {1.57, 7.654, 9.986};
         int[] arr3 = new int[2];
         arr3[0] = 21;
         arr3[1] = 22;
         //task2
-        System.out.println();
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.toString(arr3));
+        for (int i = 0; i < arr1.length; i++) {
+            if (i == arr1.length - 1) {
+                System.out.println(arr1[i]);
+                break;
+            }
+            System.out.print(arr1[i] + ", ");
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            if (i == arr2.length - 1) {
+                System.out.println(arr2[i]);
+                break;
+            }
+            System.out.print(arr2[i] + ", ");
+        }
+        for (int i = 0; i < arr3.length; i++) {
+            if (i == arr3.length - 1) {
+                System.out.println(arr3[i]);
+                break;
+            }
+            System.out.print(arr3[i] + ", ");
+        }
         //task3
-        //Подскажите пожалуйста если можно сделать проще, по-моему получилось мудрёно
         System.out.println();
-        for (int i = arr1.length; i > 0; i--) {
-            if (i == 1) {
-                System.out.println(arr1[i - 1]);
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr1[i]);
                 break;
             }
-            System.out.print(arr1[i - 1] + ", ");
+            System.out.print(arr1[i] + ", ");
         }
-        for (int i = arr2.length; i > 0; i--) {
-            if (i == 1) {
-                System.out.println(arr2[i - 1]);
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr2[i]);
                 break;
             }
-            System.out.print(arr2[i - 1] + ", ");
+            System.out.print(arr2[i] + ", ");
         }
-        for (int i = arr3.length; i > 0; i--) {
-            if (i == 1) {
-                System.out.println(arr3[i - 1]);
+        for (int i = arr3.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr3[i]);
                 break;
             }
-            System.out.print(arr3[i - 1] + ", ");
+            System.out.print(arr3[i] + ", ");
         }
         //task4
         System.out.println();
